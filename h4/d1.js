2b3ca3a84c9b6b0c230d5966f93bc44b5d0884d0
Hildur Bjarkadottir <hib24@hi.is>
2023-09-23 14:54:24 +0200

    h4

---
+++ h4/d1.js
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////////////////
+//    Sýnidæmi í Tölvugrafík
+//     Einfaldasta WebGL forritið.  Teiknar einn rauðan þríhyrning.
+//
+//    Hjálmtýr Hafsteinsson, ágúst 2023
+///////////////////////////////////////////////////////////////////
+var gl;
+var points;
+var matrixLoc;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    var vertices = new Float32Array([0, 0, 0.5, 1, 1, 0]);
+
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.9, 0.9, 0.9, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+
+    // Associate shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    matrixLoc = gl.getUniformLocation( program, "rotation" );
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    var mv = mat4();
+
+    mv = mult( mv, translate( -0.5, -0.5, 0.0 ) );
+
+    mv = mult( mv, rotateZ( 90 ) );
+    mv = mult( mv, scalem( 0.5, 1.0, 1.0) );
+
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
+
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+}
